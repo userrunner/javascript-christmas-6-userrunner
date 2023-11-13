1cf1fbc79e6fc81184e6c6d74fc98c23960dd95f
chorong <chorong4641@naver.com>
2023-11-13 22:17:27 +0900

    feat(input): 방문 날짜 및 메뉴 입력 시 오류가 발생하면 재입력 받도록 구현

--- src/InputView.js
+++ src/InputView.js
@@ -3,14 +3,28 @@ import VisitDate from "./VisitDate.js";
 import Order from "./Order.js";
 
 const InputView = {
-    async readDate() {
+  async readDate() {
+    while (true) {
+      try {
         const dateInput = await Console.readLineAsync("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n");
         const visitDate = new VisitDate(parseInt(dateInput, 10));
-    },
-    async readMenu() {
+        return visitDate.getVisitDate();
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+  },
+  async readMenu() {
+    while (true) {
+      try {
         const menuInput = await Console.readLineAsync("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)\n");
         const order = new Order(menuInput);
+        return order;
+      } catch (error) {
+        Console.print(error.message);
+      }
     }
+  }
 };
 
 export default InputView;
