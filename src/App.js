ab3fe7c794859560ddd6f1bf0e41a7162fe041ca
chorong <chorong4641@naver.com>
2023-11-13 23:35:21 +0900

    feat(output): 현재까지 구현한 기능을 토대로 메시지 출력

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./InputView.js";
+import Order from "./Order.js";
+import VisitDate from "./VisitDate.js";
+import Discount from "./Discount.js";
+import OutputView from "./OutputView.js";
+
 class App {
-  async run() {}
+  async run() {
+    OutputView.printIntroduce();
+    const inputDate = await InputView.readDate();
+    const order = await InputView.readMenu();
+    OutputView.printEventPreview();
+    OutputView.printMenu(order);
+
+    const discount = new Discount(inputDate, order);
+    OutputView.printBeforeTotalAmount(order.getTotalAmount());
+  }
 }
 
 export default App;
--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,23 @@
-export default OutputView = {
-    printMenu() {
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+    printIntroduce() {
+        Console.print("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
+    },
+    printEventPreview() {
+        Console.print("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n");
+    },
+    printMenu(order) {
+        const { orderMenuNames, orderMenuCounts } = order.getOrderInfo();
         Console.print("<주문 메뉴>");
-        // ...
+        orderMenuNames.map((menuName, index) => {
+            Console.print(`${menuName} ${orderMenuCounts[index]}개`);
+        });
+    },
+    printBeforeTotalAmount(amount) {
+        Console.print("\n<할인 전 총주문 금액>");
+        Console.print(`${amount.toLocaleString('ko-KR')}원`);
     }
-    // ...
-}
+};
+
+export default OutputView;
