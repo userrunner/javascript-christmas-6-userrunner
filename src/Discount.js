76ffe8aa163d63901bfca584dc0894c8ceca9bef
chorong <chorong4641@naver.com>
2023-11-13 22:15:43 +0900

    fix(discount): 혜택 금액 반환 시 변수 설정 오류 수정

--- src/Discount.js
+++ src/Discount.js
@@ -1,41 +1,43 @@
 class Discount {
   constructor(visitDate, order) {
-    this.#calculateDiscount(visitDate, order);
+    this.calculateDiscount(visitDate, order);
   }
 
-  #calculateDiscount(visitDate, order) {
+  calculateDiscount(visitDate, order) {
     const newDate = new Date(`2023-12-${visitDate}`);
     const mainCount = order.getDayDiscountMenuCount('main');
     const dessertCount = order.getDayDiscountMenuCount('dessert');
     const week = newDate.getDay();
 
-    this.#calculateWeekdayDiscount(week, dessertCount);
-    this.#calculateHolidayDiscount(week, mainCount);
-    this.#calculateDDayDiscount(visitDate);
-    this.#calculateSpecialDiscount(week, visitDate);
+    const weekdayDiscount = this.#calculateWeekdayDiscount(week, dessertCount) || 0;
+    const holidayDiscount = this.#calculateHolidayDiscount(week, mainCount) || 0;
+    const dDayDiscount = this.#calculateDDayDiscount(visitDate) || 0;
+    const specialDiscount = this.#calculateSpecialDiscount(week, visitDate) || 0;
+
+    return { weekdayDiscount, holidayDiscount, dDayDiscount, specialDiscount };
   }
 
   #calculateWeekdayDiscount(week, dessertCount) {
     if (week <= 4) {
-      return weekdayDiscount = dessertCount * 2023;
+      return dessertCount * 2023;
     }
   }
 
   #calculateHolidayDiscount(week, mainCount) {
     if (week >= 5) {
-      return holidayDiscount = mainCount * 2023;
+      return mainCount * 2023;
     }
   }
 
   #calculateDDayDiscount(visitDate) {
     if (visitDate <= 25) {
-      return dDayDiscount = 1000 + ((visitDate - 1) * 100);
+      return 1000 + ((visitDate - 1) * 100);
     }
   }
 
   #calculateSpecialDiscount(week, visitDate) {
     if (week === 0 || visitDate === 25) {
-      return specialDiscount = 1000;
+      return 1000;
     }
   }
 }
